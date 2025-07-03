aeace58932272a300aa007e08aaaaca465b42bb3
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-03 07:55:36 +0200

    chore(api): generate migrations after model update

---
+++ apps/api/src/db/migrations/0000_charming_nuke.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `tokens` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`issued_at` integer NOT NULL,
+	`expires_at` integer NOT NULL,
+	`revoked_at` integer,
+	`user_agent` text,
+	`ip` text
+);
+--> statement-breakpoint
+CREATE TABLE `user_roles` (
+	`user_id` text NOT NULL,
+	`role` text DEFAULT 'user' NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`password` text NOT NULL,
+	`avatar` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
--- apps/api/src/db/migrations/0002_early_squirrel_girl.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX `tokens_refresh_token_unique`;--> statement-breakpoint
-ALTER TABLE `tokens` DROP COLUMN `refresh_token`;
