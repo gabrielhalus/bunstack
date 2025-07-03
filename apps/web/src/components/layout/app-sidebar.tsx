dc3718d21beb501dcdae23cd0450fce9fa3a3de2
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-03 11:09:57 +0200

    feat(web): update app description

--- apps/web/src/components/layout/app-sidebar.tsx
+++ apps/web/src/components/layout/app-sidebar.tsx
@@ -36,7 +36,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">Bunstack.</span>
-                  <span className="truncate text-xs">Minecraft Server Manager</span>
+                  <span className="truncate text-xs">BHVR Starter Kit</span>
                 </div>
               </Link>
             </SidebarMenuButton>
