9611dbc4dd00d64de93ede795ca780bea2002d71
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-04 12:04:34 +0200

    feat(shared): add primary key to `user_role`

--- packages/shared/src/schemas/users/relations.ts
+++ packages/shared/src/schemas/users/relations.ts
@@ -1,8 +1,10 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { primaryKey, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 import { ROLES } from "./types";
 
 export const userRoles = sqliteTable("user_roles", {
   userId: text("user_id").notNull(),
   role: text("role", { enum: ROLES }).notNull().default("user"),
-});
+}, table => [
+  primaryKey({ columns: [table.userId, table.role] }),
+]);
