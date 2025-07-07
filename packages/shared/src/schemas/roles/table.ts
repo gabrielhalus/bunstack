9211692aeb833a4978026d79cb0ac0c063d90efc
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-07 07:20:43 +0200

    feat(shared): add `index` column on `roles` model

--- packages/shared/src/schemas/roles/table.ts
+++ packages/shared/src/schemas/roles/table.ts
@@ -5,10 +5,12 @@ import { nanoid } from "nanoid";
 export const roles = sqliteTable("roles", {
   id: text("id").primaryKey().$defaultFn(() => nanoid()),
   name: text("name").notNull(),
+  index: integer("index").notNull().default(0),
   default: integer("default", { mode: "boolean" }).notNull().default(false),
   superUser: integer("super_user", { mode: "boolean" }).notNull().default(false),
   createdAt: integer("created_at").notNull().$defaultFn(() => Date.now()),
   updatedAt: integer("updated_at").notNull().$defaultFn(() => Date.now()),
 }, t => [
+  uniqueIndex("index_unique").on(t.index),
   uniqueIndex("default_unique").on(t.default).where(sql`"default" = 1`),
 ]);
