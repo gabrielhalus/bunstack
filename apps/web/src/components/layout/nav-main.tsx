e49a4c5aa91ad0896ffc697da406e85a455dcaf0
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-07 06:47:55 +0200

    style(web): fix lint issues

--- apps/web/src/components/layout/nav-main.tsx
+++ apps/web/src/components/layout/nav-main.tsx
@@ -1,9 +1,11 @@
-import { ChevronRight, type LucideIcon } from "lucide-react";
+import type { LinkOptions } from "@tanstack/react-router";
+import type { LucideIcon } from "lucide-react";
 
-import { Link, LinkOptions, useLocation } from "@tanstack/react-router";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible";
+import { Link, useLocation } from "@tanstack/react-router";
+import { ChevronRight } from "lucide-react";
 
 import { SidebarMenu, SidebarMenuAction, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem } from "@/components/ui/sidebar";
-import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "@radix-ui/react-collapsible";
 
 export function NavMain({ items }: { items: { title: string; icon: LucideIcon; href: LinkOptions; items?: { title: string; href: LinkOptions }[] }[] }) {
   const location = useLocation();
@@ -11,41 +13,43 @@ export function NavMain({ items }: { items: { title: string; icon: LucideIcon; h
 
   return (
     <SidebarMenu>
-      {items.map((item) => (
-          <Collapsible key={item.title} asChild defaultOpen={isActive(item.href.to)}>
-            <SidebarMenuItem>
-              <SidebarMenuButton asChild tooltip={item.title} isActive={isActive(item.href.to)}>
-                <Link {...item.href}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </Link>
-              </SidebarMenuButton>
-              {item.items?.length ? (
-                <>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuAction className="data-[state=open]:rotate-90">
-                      <ChevronRight />
-                      <span className="sr-only">Toggle</span>
-                    </SidebarMenuAction>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <SidebarMenuSub>
-                      {item.items?.map((subItem) => (
-                        <SidebarMenuSubItem key={subItem.title}>
-                          <SidebarMenuSubButton asChild>
-                          <Link {...subItem.href}>
-                              <span>{subItem.title}</span>
-                            </Link>
-                          </SidebarMenuSubButton>
-                        </SidebarMenuSubItem>
-                      ))}
-                    </SidebarMenuSub>
-                  </CollapsibleContent>
-                </>
-              ) : null}
-            </SidebarMenuItem>
-          </Collapsible>
-        ))}
+      {items.map(item => (
+        <Collapsible key={item.title} asChild defaultOpen={isActive(item.href.to)}>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild tooltip={item.title} isActive={isActive(item.href.to)}>
+              <Link {...item.href}>
+                <item.icon />
+                <span>{item.title}</span>
+              </Link>
+            </SidebarMenuButton>
+            {item.items?.length
+              ? (
+                  <>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuAction className="data-[state=open]:rotate-90">
+                        <ChevronRight />
+                        <span className="sr-only">Toggle</span>
+                      </SidebarMenuAction>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items?.map(subItem => (
+                          <SidebarMenuSubItem key={subItem.title}>
+                            <SidebarMenuSubButton asChild>
+                              <Link {...subItem.href}>
+                                <span>{subItem.title}</span>
+                              </Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </>
+                )
+              : null}
+          </SidebarMenuItem>
+        </Collapsible>
+      ))}
     </SidebarMenu>
   );
 }
--- apps/web/src/routes/_authenticated/_dashboard/settings/index.tsx
+++ apps/web/src/routes/_authenticated/_dashboard/settings/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_authenticated/_dashboard/settings/')({
+export const Route = createFileRoute("/_authenticated/_dashboard/settings/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/_dashboard/settings/"!</div>
+  return <div>Hello "/_authenticated/_dashboard/settings/"!</div>;
 }
