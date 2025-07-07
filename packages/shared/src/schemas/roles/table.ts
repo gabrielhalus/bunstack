5455b3c417911ae0f4cb29cf13dcc62c7617a75b
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-07 07:09:33 +0200

    feat(shared): add `default` and `superUser` columns to `roles` model

--- packages/shared/src/schemas/roles/table.ts
+++ packages/shared/src/schemas/roles/table.ts
@@ -1,9 +1,14 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text, uniqueIndex } from "drizzle-orm/sqlite-core";
 import { nanoid } from "nanoid";
 
 export const roles = sqliteTable("roles", {
   id: text("id").primaryKey().$defaultFn(() => nanoid()),
   name: text("name").notNull(),
+  default: integer("default", { mode: "boolean" }).notNull().default(false),
+  superUser: integer("super_user", { mode: "boolean" }).notNull().default(false),
   createdAt: integer("created_at").notNull().$defaultFn(() => Date.now()),
   updatedAt: integer("updated_at").notNull().$defaultFn(() => Date.now()),
-});
+}, t => [
+  uniqueIndex("default_unique").on(t.default).where(sql`"default" = 1`),
+]);
