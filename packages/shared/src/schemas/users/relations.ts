f2ecf86bcdea1a346ad328083451f5ac78aa7ff1
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-07 07:33:34 +0200

    feat(packages): rename `role` key -> `roleId` in `userRoles`

--- packages/shared/src/schemas/users/relations.ts
+++ packages/shared/src/schemas/users/relations.ts
@@ -2,7 +2,7 @@ import { primaryKey, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const userRoles = sqliteTable("user_roles", {
   userId: text("user_id").notNull(),
-  role: text("role_id").notNull(),
+  roleId: text("role_id").notNull(),
 }, table => [
-  primaryKey({ columns: [table.userId, table.role] }),
+  primaryKey({ columns: [table.userId, table.roleId] }),
 ]);
