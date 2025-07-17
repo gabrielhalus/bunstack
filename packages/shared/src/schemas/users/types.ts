07cf0b96c004fe1ba61909fa6b97033e06a70819
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-17 06:14:44 +0200

    feat(shared): add `id` and `label` to user nested roles

--- packages/shared/src/schemas/users/types.ts
+++ packages/shared/src/schemas/users/types.ts
@@ -1,6 +1,6 @@
 import type { Merge } from "../../common";
 import type { usersTable } from "./table";
 
-export type User = Merge<Omit<typeof usersTable.$inferSelect, "password"> & { password?: string }, { roles: string[] }>;
+export type User = Merge<Omit<typeof usersTable.$inferSelect, "password"> & { password?: string }, { roles: { id: string; label: string }[] }>;
 
 export type UserUniqueFields = Pick<User, "id" | "email">;
