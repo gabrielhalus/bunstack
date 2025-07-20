2c1eed62c693671c4bb43a791aa0da843cf7a709
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-20 22:26:29 +0200

    feat(shared): add Condition and Operand types

--- packages/shared/src/schemas/policies/types.ts
+++ packages/shared/src/schemas/policies/types.ts
@@ -1,3 +1,32 @@
 import type { Policies } from "./table";
 
 export type Policy = typeof Policies.$inferSelect;
+
+export type Operand =
+  | { type: "user_attr"; key: string }
+  | { type: "resource_attr"; key: string }
+  | { type: "literal"; value: any };
+
+export type Condition =
+  | {
+    op: "and" | "or";
+    conditions: Condition[];
+  }
+  | {
+    op: "not";
+    condition: Condition;
+  }
+  | {
+    op: "eq" | "neq" | "lt" | "lte" | "gt" | "gte";
+    left: Operand;
+    right: Operand;
+  }
+  | {
+    op: "in" | "not_in";
+    left: Operand;
+    right: Operand[];
+  }
+  | {
+    op: "exists" | "not_exists";
+    operand: Operand;
+  };
