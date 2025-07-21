4a7d3c3363c5d5245ae3b9b69a905a66dcf34a63
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-21 09:32:00 +0200

    fix(shared): add __root constant to resolve database url from root

--- packages/shared/src/db/index.ts
+++ packages/shared/src/db/index.ts
@@ -1,8 +1,10 @@
 import Sqlite from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
+import { join } from "node:path";
 import env from "../lib/env";
+import { __root } from "../constants/__root";
 
-const queryClient = new Sqlite(env.DATABASE_URL);
+const queryClient = new Sqlite(join(__root, env.DATABASE_URL));
 
 export const db = drizzle({ client: queryClient });
