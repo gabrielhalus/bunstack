58aebe122a935265239687a47eeac54b81af0ee6
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-25 12:48:33 +0200

    feat(web): scaffold role pages

--- apps/web/src/lib/api/roles.ts
+++ apps/web/src/lib/api/roles.ts
@@ -1,4 +1,4 @@
-import type { RoleWithMembersCount } from "@bunstack/shared/db/types/roles";
+import type { RoleWithMembers, RoleWithMembersCount } from "@bunstack/shared/db/types/roles";
 
 import { fetchAuthenticated } from "./http";
 
@@ -8,4 +8,12 @@ export async function getAllRoles(): Promise<RoleWithMembersCount[]> {
     throw new Error("Failed to get roles");
   }
   return res.json().then(data => data.roles);
+}
+
+export async function getRoleByName(name: string): Promise<RoleWithMembers> {
+  const res = await fetchAuthenticated(`/api/roles/${name}`);
+  if (!res.ok) {
+    throw new Error("Failed to get role");
+  }
+  return res.json().then(data => data.role);
 }
