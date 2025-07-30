a6bf5a9107635ed1afd78b8959f58646489b0fcc
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-30 15:57:03 +0200

    feat(web): add common and app translations

---
+++ apps/web/src/i18n/index.ts
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpBackend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: ["en", "fr"],
+    ns: ["common", "dashboard"],
+    defaultNS: "common",
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: true,
+    },
+  });
+
+export default i18n;
