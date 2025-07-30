309d2c78d0936a7a9fc0cf5a6286da75025b5fe9
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-30 11:39:44 +0200

    feat(web): handle user deletion

--- apps/web/src/routes/_authenticated/_dashboard/users/-components/action-dropdown.tsx
+++ apps/web/src/routes/_authenticated/_dashboard/users/-components/action-dropdown.tsx
@@ -1,19 +1,39 @@
 import type { User } from "@bunstack/shared/db/types/users";
 import type { Row } from "@tanstack/react-table";
 
-import { Copy, MoreHorizontal, Trash } from "lucide-react";
+import { Copy, Loader2, MoreHorizontal, Trash } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { TimeoutButton } from "@/components/ui/timeout-button";
 import { useAuth } from "@/hooks/use-auth";
+import { deleteUser } from "@/lib/api/users";
+import { getAllUsersQueryOptions } from "@/lib/queries/users";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export function ActionDropdown({ row }: { row: Row<User> }) {
   const { can, loading } = useAuth();
-
+  const queryClient = useQueryClient();
+  
   if (loading) {
     return null;
   }
+
+  const user = row.original;
+
+  const mutation = useMutation({
+    mutationFn: deleteUser,
+    onError: () => {
+      toast.error("Failed to delete user");
+    },
+    onSuccess: () => {
+      toast.success("User deleted");
+
+      queryClient.setQueryData(getAllUsersQueryOptions.queryKey, (existingUsers) => {
+        return existingUsers?.filter((u) => u.id !== user.id) ?? [];
+      });
+    },
+  })
 
   return (
     <DropdownMenu>
@@ -25,15 +45,15 @@ export function ActionDropdown({ row }: { row: Row<User> }) {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(row.original.id)}>
+        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(user.id)}>
           <Copy className="h-4 w-4" />
           Copy User ID
         </DropdownMenuItem>
         {can("user:delete") && (
-          <TimeoutButton variant="destructive" size="sm" noExpansion timeout={2000} onClick={() => ({})}>
-            <Trash className="h-4 w-4" />
+          <Button disabled={mutation.isPending} onClick={() => mutation.mutate(user)} variant="destructive" size="sm">
+            {mutation.isPending ? <Loader2 className="h-4 w-4 animate-spin" /> : <Trash className="h-4 w-4" />}
             Delete User
-          </TimeoutButton>
+          </Button>
         )}
       </DropdownMenuContent>
     </DropdownMenu>
