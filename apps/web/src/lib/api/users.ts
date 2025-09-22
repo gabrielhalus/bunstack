16938b0a6ada27e4bb573effbf0cfa2880cc7754
Gabriel Halus <gabrielhalus@gmail.com>
2025-09-22 13:31:19 +0200

    feat(web): update calls to `/api/users`

--- apps/web/src/lib/api/users.ts
+++ apps/web/src/lib/api/users.ts
@@ -1,49 +1,24 @@
-import type { User, UserWithRoles } from "@bunstack/shared/db/types/users";
+import { api } from "../http";
 
-import { fetchAuthenticated } from "@/lib/api/http";
-
-export async function getAllUsers(): Promise<{ users: UserWithRoles[]; total: number }> {
-  const res = await fetchAuthenticated("/api/users");
-
-  if (!res.ok) {
-    throw new Error("Failed to get users");
-  }
-
-  return await res.json();
+export async function getAllUsers() {
+  const res = await api.users.$get({ query: {} });
+  return res.json();
 }
 
-export async function getUsersPaginated({ page = 0, pageSize = 10, search, sortField, sortDirection }: { page?: number; pageSize?: number; search?: string; sortField?: string; sortDirection?: "asc" | "desc" }): Promise<{ users: UserWithRoles[]; total: number }> {
-  const params = new URLSearchParams({
-    page: page.toString(),
-    pageSize: pageSize.toString(),
-  });
+export async function getUsersPaginated({ page = 0, pageSize = 10, search, sortField, sortDirection }: { page?: number; pageSize?: number; search?: string; sortField?: string; sortDirection?: "asc" | "desc" }) {
+  const params = {
+    page: page?.toString(),
+    pageSize: pageSize?.toString(),
+    search,
+    sortField,
+    sortDirection,
+  };
 
-  if (search) {
-    params.append("search", search);
-  }
-  if (sortField) {
-    params.append("sortField", sortField);
-  }
-  if (sortDirection) {
-    params.append("sortDirection", sortDirection);
-  }
-
-  const res = await fetchAuthenticated(`/api/users?${params}`);
-  if (!res.ok) {
-    throw new Error("Failed to get users");
-  }
-
-  return await res.json();
+  const res = await api.users.$get({ query: params });
+  return res.json();
 }
 
-export async function deleteUser({ id }: { id: string }): Promise<{ user: User }> {
-  const res = await fetchAuthenticated(`/api/users/${id}`, {
-    method: "DELETE",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to delete user");
-  }
-
-  return await res.json();
+export async function deleteUser({ id }: { id: string }) {
+  const res = await api.users[":id"].$delete({ param: { id } });
+  return res.json();
 }
