74513276829bc4f0a22b1daf57265c8092c8ffbe
Gabriel Halus <gabrielhalus@gmail.com>
2025-10-02 19:38:11 +0200

    feat(shared): update password rules definition

--- packages/shared/src/contracts/auth/schemas.ts
+++ packages/shared/src/contracts/auth/schemas.ts
@@ -1,12 +1,29 @@
 import { z } from "zod";
 
+export const passwordRules = {
+  minLength: 8,
+  minUppercase: 1,
+  minLowercase: 1,
+  minDigits: 1,
+  minSpecialChars: 1,
+};
+
+export const passwordChecks = {
+  minLength: (val: string) => val.length >= passwordRules.minLength,
+  minUppercase: (val: string) => (val.match(/[A-Z]/g) || []).length >= passwordRules.minUppercase,
+  minLowercase: (val: string) => (val.match(/[a-z]/g) || []).length >= passwordRules.minLowercase,
+  minDigits: (val: string) => (val.match(/\d/g) || []).length >= passwordRules.minDigits,
+  minSpecialChars: (val: string) =>
+    (val.match(/[!@#$%^&*()\-=+[\]{};:'",.<>/?\\|`]/g) || []).length >= passwordRules.minSpecialChars,
+};
+
 export const passwordSchema = z
   .string()
-  .min(8, "minLengthErrorMessage")
-  .regex(/[A-Z]/, "uppercaseErrorMessage")
-  .regex(/[a-z]/, "lowercaseErrorMessage")
-  .regex(/\d/, "numberErrorMessage")
-  .regex(/[!@#$%^&*()\-=+[\]{};:'",.<>/?\\|`]/, "specialCharacterErrorMessage");
+  .min(passwordRules.minLength, "minLengthErrorMessage")
+  .refine(passwordChecks.minUppercase, "minUppercaseErrorMessage")
+  .refine(passwordChecks.minLowercase, "minLowercaseErrorMessage")
+  .refine(passwordChecks.minDigits, "minDigitsErrorMessage")
+  .refine(passwordChecks.minSpecialChars, "minSpecialCharsErrorMessage");
 
 export const registerInputSchema = z.object({
   name: z.string().min(1, "requiredErrorMessage").min(3, "minLengthErrorMessage").max(20, "maxLengthErrorMessage"),
---
+++ packages/shared/src/contracts/auth/types.ts
@@ -0,0 +1,7 @@
+export type PasswordRules = {
+  minLength?: number;
+  minUppercase?: number;
+  minLowercase?: number;
+  minDigits?: number;
+  minSpecialChars?: number;
+};
