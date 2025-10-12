fec98bc8e441d3be3180f599ab76bf8172d34231
Gabriel Halus <gabrielhalus@gmail.com>
2025-10-12 19:44:57 +0200

    feat(web): add `I18nextProvider`

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
 
 import i18n from "@/i18n";
 import { routeTree } from "@/routeTree.gen";
@@ -34,11 +35,13 @@ async function bootstrap() {
 
   root.render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <Toaster position="bottom-center" />
-        <Sayno />
-      </QueryClientProvider>
+      <I18nextProvider i18n={i18n}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <Toaster position="bottom-center" />
+          <Sayno />
+        </QueryClientProvider>
+      </I18nextProvider>
     </StrictMode>,
   );
 }
