f57f4b88badc09a6021cdb5861d74d9af3c6fce7
Gabriel Halus <gabrielhalus@gmail.com>
2025-12-12 10:11:05 +0100

    chore(repo): remove `apps/site`

--- apps/site/src/routes/__root.tsx
+++
@@ -1,9 +0,0 @@
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-
-export const Route = createRootRouteWithContext()({
-  component: RootLayout,
-});
-
-function RootLayout() {
-  return <Outlet />;
-}
